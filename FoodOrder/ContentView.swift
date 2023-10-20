f287b7daba5486e2c6813e55139919fb65fd1609
Kumani K <kumanikidd@gmail.com>
2023-10-20 18:45:49 +0100

    Initial Commit

---
+++ FoodOrder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodOrder
+//
+//  Created by Kumani Kidd on 20/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodOrder/FoodOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderApp.swift
+//  FoodOrder
+//
+//  Created by Kumani Kidd on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
